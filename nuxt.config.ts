e71dd77ad89def4384b4810bc29b477685e4ea63
Liang <liang@in-x.cc>
2025-06-16 22:28:36 +0800

    devtools watch 太多文件导致无法启动 nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ["@nuxt/eslint"],
-  nitro: {
-    watchOptions: {
-      ignored: [".data"],
-    },
-  },
 });
